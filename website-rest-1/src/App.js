ad0f11b45ab7a1226c1e585318bd66ec8907c88d
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-05-31 11:18:07 +0200

    ajout font

--- website-rest-1/src/App.js
+++ website-rest-1/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home"
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
---
+++ website-rest-1/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from '../../assets/kisspng-wen-ming-house-ii-chinese-cuisine-restaurant-crawf-fortuna-garden-chinese-restaurant-5b06b93226c0d7.6493265115271672821588.png'
+import LogoMarket from "../../assets/panier.svg"
+import LogoAccount from "../../assets/compte.svg"
+
+function Navbar(){
+    return(
+        <div className="container-navbar">
+            <img className="logo" src={Logo} alt="" />
+
+            <ul className="list-links">
+                <li className="link">Accueil</li>
+                <li className="link">Carte</li>
+                <li className="link">Réservation</li>
+            </ul>
+
+            <ul className="list-actions">
+                <li className="action"><img src={LogoMarket} alt="" /></li>
+                <li className="action"><img src={LogoAccount} alt="" /></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
