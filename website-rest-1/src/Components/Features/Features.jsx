dea47e5858437b798957e56b6d22f4e375167120
Aghiles Haddar <haddaraghiles5@gmail.com>
2023-05-31 16:01:10 +0200

    correction

--- website-rest-1/src/Components/Features/Features.jsx
+++ website-rest-1/src/Components/Features/Features.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './Features.css'
 import symbol1 from '../../assets/symbol1-features.svg'
+import symbol2 from '../../assets/symbol2-features.svg'
+import symbol3 from '../../assets/symbol3-features.svg'
 
 function Features(){
     return(
@@ -15,15 +17,15 @@ function Features(){
                 </div>
 
                 <div className="features-card">
-                    <img className="features-card-img" src={symbol1} alt="" />
-                    <h2 className="features-card-title">Produit frais</h2>
-                    <p className="features-card-p">Nos poissons sont pêchés le <br /> jours même</p>
+                    <img className="features-card-img" src={symbol2} alt="" />
+                    <h2 className="features-card-title">Service rapide</h2>
+                    <p className="features-card-p">Nos équipes répondrons à <br /> vos attentes dans les plus <br /> bref délai</p>
                 </div>
 
                 <div className="features-card">
-                    <img className="features-card-img" src={symbol1} alt="" />
-                    <h2 className="features-card-title">Produit frais</h2>
-                    <p className="features-card-p">Nos poissons sont pêchés le <br /> jours même</p>
+                    <img className="features-card-img" src={symbol3} alt="" />
+                    <h2 className="features-card-title">Buffet à volonté</h2>
+                    <p className="features-card-p">Une multitude de choix rien <br /> que pour vous</p>
                 </div>
 
                 
