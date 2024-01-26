38f3bd262fabea9c3913e739f18d7c94caa7ac84
dwain93 <74243765+dwain93@users.noreply.github.com>
2024-01-26 14:26:23 +0100

    modif

--- website-rest-1/src/API/apiService.jsx
+++ website-rest-1/src/API/apiService.jsx
@@ -148,10 +148,9 @@ export const apiService = {
                 },
                 body: JSON.stringify(credentials),
             });
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        } else {
             return await response.json();
+        } catch (error) {
+            throw error;
         }
     },
 
